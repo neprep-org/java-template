d7a4202023c64dc1c1944229dbefb4926f18bf03
Murangwa Pacifique <pacifiquemurangwa001@gmail.com>
2024-06-24 21:40:34 +0200

    feat: java template for ne

---
+++ src/main/java/com/rca/myspringsecurity/MySpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.rca.myspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rca/myspringsecurity/MySpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rca.myspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
